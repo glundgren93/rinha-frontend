42912c28a984fd400dcce1f3757020b7b5f900f3
Gabriel Lundgren <glundgren93@gmail.com>
2023-10-15 21:14:50 +0200

    improving things a bit

--- index.js
+++ index.js
@@ -6,93 +6,46 @@ document
 function openDialog() {
   document.getElementById("json-file").click();
 }
-
 const myWorker = new Worker("/worker.js");
-
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker
-//     .register("/sw.js")
-//     .then(function (registration) {
-//       console.log("Service Worker registered with scope:", registration.scope);
-//     })
-//     .catch(function (error) {
-//       console.error("Service Worker registration failed:", error);
-//     });
-// }
-
-// function sendToServiceWorker(arrayBuffer) {
-//   if ("serviceWorker" in navigator && navigator.serviceWorker.controller) {
-//     const messageChannel = new MessageChannel();
-//     messageChannel.port1.onmessage = handleServiceWorkerResponse;
-
-//     myWorker.postMessage([arrayBuffer]);
-
-//     navigator.serviceWorker.controller.postMessage(
-//       { command: "processArrayBuffer", arrayBuffer: arrayBuffer },
-//       [arrayBuffer, messageChannel.port2]
-//     );
-//   }
-// }
-
-function handleServiceWorkerResponse(event) {
-  const prettifiedJson = event.data.prettifiedJson;
-  document.getElementById("json-tree").innerHTML = prettifiedJson;
-}
 
 function handleFileSubmission(event) {
   const file = event.target.files[0];
 
   if (file) {
-    // parseFile(file);
     const reader = new FileReader();
 
     reader.onload = function (event) {
-      myWorker.postMessage([event.target.result]);
+      myWorker.postMessage({
+        action: "processTree",
+        tree: event.target.result,
+      });
     };
 
     reader.readAsArrayBuffer(file);
   }
 }
 
-// function parseFile(file, callback) {
-//   var fileSize = file.size;
-//   var chunkSize = 64 * 1024; // bytes
-//   var offset = 0;
-//   var self = this; // we need a reference to the current object
-//   var chunkReaderBlock = null;
-
-//   var readEventHandler = function (evt) {
-//     if (evt.target.error == null) {
-//       offset += evt.target.result.byteLength;
-//       sendToServiceWorker(evt.target.result); // callback for handling read chunk
-//     } else {
-//       console.log("Read error: " + evt.target.error);
-//       return;
-//     }
-//     if (offset >= fileSize) {
-//       console.log("Done reading file");
-//       return;
-//     }
-
-//     // of to the next chunk
-//     chunkReaderBlock(offset, chunkSize, file);
-//   };
-
-//   chunkReaderBlock = function (_offset, length, _file) {
-//     var r = new FileReader();
-//     var blob = _file.slice(_offset, length + _offset);
-//     r.onload = readEventHandler;
-//     r.readAsArrayBuffer(blob);
-//   };
-
-//   // now let's start the read with the first block
-//   chunkReaderBlock(offset, chunkSize, file);
-// }
-
-myWorker.onmessage = (e) => {
-  let html = "";
-  // Object.keys(e.data).forEach((key) => {
-  //   html += `<span>${key}</span><span>${e.data[]}</span>`
-  // });
-  document.getElementById("json-tree").innerHTML = e.data;
+myWorker.onmessage = function (event) {
+  if (event.data.action === "processedTree") {
+    myWorker.postMessage({ action: "getNextChunk" });
+  }
+  if (event.data.action === "newChunk") {
+    if (!event.data.done) {
+      renderData(event.data.chunk);
+    }
+    // Optionally, handle the 'done' state. E.g., hide a loading spinner, display a message, etc.
+  }
 };
+
+window.addEventListener("scroll", () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
+    myWorker.postMessage({ action: "getNextChunk" });
+  }
+});
+
+function renderData(chunk) {
+  const container = document.getElementById("json-tree");
+  container.style.display = "flex";
+  document.querySelector(".content").style.display = "none";
+  container.innerHTML += chunk.join("\n");
+}
--- worker.js
+++ worker.js
@@ -1,62 +1,70 @@
-onmessage = async (e) => {
-  console.log("Message received from main script");
-  postMessage(await decode(e.data[0]));
+let allNodes = [];
+let generatorInstance = null;
+
+onmessage = async function (event) {
+  switch (event.data.action) {
+    case "processTree":
+      console.time("processTree");
+      allNodes = await decode(event.data.tree);
+      generatorInstance = treeGenerator(allNodes);
+      console.timeEnd("processTree");
+      postMessage({
+        action: "processedTree",
+      });
+      break;
+    case "getNextChunk":
+      const nextChunk = await generatorInstance.next();
+      postMessage({
+        action: "newChunk",
+        chunk: nextChunk.value,
+        done: nextChunk.done,
+      });
+      break;
+  }
 };
 
 async function decode(arrayBuffer) {
   const decoder = new TextDecoder();
-
   const jsonStr = decoder.decode(arrayBuffer);
-  return await recurseTree(JSON.parse(jsonStr), 0);
+
+  const parsedJson = await recurseTree(JSON.parse(jsonStr));
+  return parsedJson;
 }
 
-async function recurseTree(tree, indentLevel = 0) {
-  const indent = "  ".repeat(indentLevel); // For visual indentation in the HTML output.
+async function* treeGenerator(tree, chunkSize = 10) {
+  const allNodes = tree;
+  let index = 0;
 
+  while (index < allNodes.length) {
+    yield allNodes.slice(index, index + chunkSize);
+    index += chunkSize;
+  }
+}
+
+async function recurseTree(tree) {
   const tasks = Object.entries(tree).map(async ([key, value]) => {
     if (value && typeof value === "object" && !Array.isArray(value)) {
-      const nested = await recurseTree(value, indentLevel + 1); // Wait for nested formatting to be completed.
-      return `<div class="key">${key}:</div>\n${nested}`;
+      const nested = await recurseTree(value);
+      return `<div class="node"><div class="key">${key}:</div>\n<div class="leaf">${nested.join(
+        "\n"
+      )}</div></div>`;
     } else if (typeof value === "object" && Array.isArray(value)) {
-      const nested = await recurseTree(value, indentLevel + 1); // Wait for nested formatting to be completed.
-      return `<div class="entry"><div class="key">${key}:[\n${nested}]</div></div>`;
+      const nested = await recurseTree(value);
+      return `<div class="array-node"><div class="key">${key}:[\n${nested.join(
+        "\n"
+      )}]</div></div>`;
     } else {
-      return `<div class="entry"><span class="key">${key}:</span> <span class="value">${formatDisplayValue(value)}</span></div>`;
+      return `<div class="leaf"><span class="key">${key}:</span> <span class="value">${value}</span></div>`;
     }
   });
 
   const nodes = await Promise.all(tasks);
-  return nodes.join("\n");
+  return nodes;
 }
-
-    // async function recurseTree(tree, indentLevel = 0) {
-    //   const indent = "  ".repeat(indentLevel); // Assuming 2 spaces for each level of indentation.
-
-    //   const tasks = Object.entries(tree).map(async ([key, value]) => {
-    //     if (value && typeof value === "object" && !Array.isArray(value)) {
-    //       const nested = await recurseTree(value, indentLevel + 1); // Wait for nested formatting to be completed.
-    //       return `${indent}<div class="key">${key}:</div>${nested}`;
-    //     } else if (Array.isArray(value)) {
-    //       const nested = await recurseTree(value, indentLevel + 1); // Wait for nested formatting to be completed.
-    //       return `${indent}${key}:[\n${nested}\n${indent}]`;
-    //     } else {
-    //       return `${indent}<div class="entry"><span class="key">${key}: </span> <span class="value">${formatDisplayValue(
-    //         value
-    //       )}</span></div>`;
-    //     }
-    //   });
-
-    //   const nodes = await Promise.all(tasks);
-    //   return nodes.join("\n");
-    // }
 
 function formatDisplayValue(value) {
   if (typeof value === "string") {
     return `"${value}"`;
   }
   return value;
-}
-
-function applyIndentation(value, indentAmount) {
-  return "\t".repeat(indentAmount) + value;
 }
